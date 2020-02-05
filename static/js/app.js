2d07d718d2bf3d8476fcf5bef59ae4bdf387d8c0
josephsy96 <josephsy96@hotmail.com>
2020-02-05 17:30:17 -0600

    nest in json

--- static/js/app.js
+++ static/js/app.js
@@ -15,26 +15,28 @@ d3.json("json_data/samples.json").then(function(sample_data) {
 
     let sample = sample_data.samples;
         samples_array.push(sample);
+
+    // init();
+
+
+    //Append data into dropdown list
+
+    function append_dropdown(id, value) {
+        
+        let sel_data = d3.select(id);
+
+        sel_data.html(" ");
+
+        value.forEach((n) => {
+            console.log(n);
+            let opt = sel_data.append("options")
+                opt.text(n);
+        });
+    }
+
+    //Load dropdown
+    append_dropdown("#selDataset", names_array);
 });
 
-// init();
 
-
-//Append data into dropdown list
-
-function append_dropdown(id, value) {
-    
-    let sel_data = d3.select(id);
-
-    sel_data.html(" ");
-
-    value.forEach((n) => {
-        console.log(n);
-        let opt = sel_data.append("options")
-            opt.text(n);
-    });
-}
-
-//Load dropdown
-append_dropdown("#selDataset", names_array);
 
