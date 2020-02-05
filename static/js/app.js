7910f325ae8ee226dc606d4dfb0c2cb4568e6a4c
josephsy96 <josephsy96@hotmail.com>
2020-02-04 19:00:23 -0600

    append names value

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,31 @@
+//Data arrays
+const names_array = [];
+let metadata_array = [];
+let samples_array = [];
+
 //Import samples.json
 d3.json("json_data/samples.json").then(function(sample_data) {
     
-    console.log(sample_data);
-    let sample = sample_data.samples
+    //Push Objects into universal arrays
+    let names = sample_data.names;
+        names_array.push(names);
 
-    let top_10 = sample.sort(function(a,b) {
-        a.id - b.id});
+    let meta = sample_data.metadata;
+        metadata_array.push(meta);
 
-    console.log(top_10);
+    let sample = sample_data.samples;
+        samples_array.push(sample);
+});
+    
 
-    let id_total = sample.reduce((a,b) => a + b, 0);
+//Append data into dropdown list
+let $select_data = $('#selDataset');
 
-        console.log(id_total);
+$.each(names_array, function(key,value) {
+    let $select = $("<select/>", {
+        value: value,
+        text: value
     });
+    $select_data.append($select);
+});
 
