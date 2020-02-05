4a775b9b25b21a31d0ac2790722f09dd7fa3ae58
josephsy96 <josephsy96@hotmail.com>
2020-02-05 17:05:32 -0600

    append data

--- static/js/app.js
+++ static/js/app.js
@@ -23,30 +23,29 @@ d3.json("json_data/samples.json").then(function(sample_data) {
 //Append data into dropdown list
 d3.selectAll("#selDataset").on("change", optionChanged);
 
-function optionChanged() {
-    let sel_data = d3.selectAll("select")
-    names_array.forEach((n) => {
-        let opt = sel_data.append("options");
-        Object.entries(n).forEach(([key,value]) => {
-            opt.text(value);
-        });
-    });
+// function optionChanged() {
+//     let sel_data = d3.selectAll("select")
+//     names_array.forEach((n) => {
+//         let opt = sel_data.append("options");
+//         Object.entries(n).forEach(([key,value]) => {
+//             opt.text(value);
+//         });
+//     });
 
 
 
 
-    // for (let n_i = 0; names_array.length; n_i++) {
-    //     let drop_sel = names_array[n_i];
-    //     let select = d3.select("select");
-    //     select.text = drop_sel;
-    //     select.value = drop_sel;
-    //     select_data.append(select);
-    }
+//     // for (let n_i = 0; names_array.length; n_i++) {
+//     //     let drop_sel = names_array[n_i];
+//     //     let select = d3.select("select");
+//     //     select.text = drop_sel;
+//     //     select.value = drop_sel;
+//     //     select_data.append(select);
+//     }
 
 let sel_data = d3.selectAll("select")
+
     names_array.forEach((n) => {
         let opt = sel_data.append("options");
-        Object.entries(n).forEach(([key,value]) => {
-            opt.text(value);
-        });
+            opt.text(n);
     });
