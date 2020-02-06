86a86b8e1b754df06314457a868300310cd579f1
josephsy96 <josephsy96@hotmail.com>
2020-02-05 19:10:30 -0600

    add lists again

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 //Data arrays
-let names_array = names;
-let metadata_array = meta;
-let samples_array = sample;
+let names_array = [];
+let metadata_array = [];
+let samples_array = [];
 
 
 //Import samples.json
@@ -9,13 +9,13 @@ d3.json("json_data/samples.json").then(function(sample_data) {
 
     //Push Objects into universal arrays
     let names = sample_data.names;
-        // names_array.push(names);
+        names_array.push(names);
 
     let meta = sample_data.metadata;
         // metadata_array.push(meta);
 
     let sample = sample_data.samples;
-        // samples_array.push(sample);
+        samples_array.push(sample);
     });
 //=================================================================
     //log arrays 
