9337e22e525da6cdc4c12901b2a753692d0dee79
josephsy96 <josephsy96@hotmail.com>
2020-02-06 17:55:18 -0600

    test push

--- static/js/app.js
+++ static/js/app.js
@@ -3,21 +3,15 @@
 let belly_data = d3.json("json_data/samples.json").then(function(sample_data) {
 
     //Push Objects into universal arrays
-    let data = sample_data;
-        
-    return data;
-});
-
-//Declare global variables
-let names = belly_data.names;
-let meta = belly_data.metadata;
-let sample = belly_data.samples;
-
+    let names = sample_data.names;
+    let meta = sample_data.metadata;
+    let sample = sample_data.samples;   
+    
 //=================================================================
     //log arrays 
     console.log(names);
     console.log(meta);
-    console.log(`${sample}`);
+    console.log(sample);
 //=================================================================
 
     // //slice array so it's not a long line...
@@ -40,44 +34,32 @@ let sample = belly_data.samples;
     // console.log(v_names_array);
 //=================================================================
     //Append data into dropdown list
-    // function append_dropdown(value) {
+    function append_dropdown(value) {
         
-    //     let sel_data = d3.select("#selDataset");
+        let sel_data = d3.select("#selDataset");
 
-    //     value.forEach((n) => {
-    //         // console.log(n);
-    //         let opt = sel_data.append("option");
-    //             opt.text(n);
-    //     });
-    // }
+        value.forEach((n) => {
+            // console.log(n);
+            let opt = sel_data.append("option");
+                opt.text(n);
+        });
+    }
 
     // //Load dropdown
-    // append_dropdown(names);
+    append_dropdown(names);
 
+    let selection = d3.select("#selDataset");
+    //Update values on change
+    function change_value(v) {
+        let selection_value = v.value;
+        console.log(selection_value);
+    }
+
+    change_value(selection);
+
+});
     
 
 
 
- //Append data into dropdown list
-//  function append_dropdown(value) {
-        
-//     let sel_data = d3.select("#selDataset");
-
-//     value.forEach((n) => {
-//         // console.log(n);
-//         let opt = sel_data.append("option");
-//             opt.text(n);
-//     });
-// }
-
-// //Load dropdown
-// append_dropdown(names);
-
-
-let selection = d3.select("#selDataset");
-//Update values on change
-function optionChanged(v) {
-    let selection_value = v.value;
-    console.log(selection_value);
-}
 
