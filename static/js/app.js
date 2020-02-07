b94f32872145a1e6824ebc258d48c10fba45bca8
josephsy96 <josephsy96@hotmail.com>
2020-02-07 16:25:57 -0600

    add files

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 
 //Import samples.json
-function optionChanged() {
+function belly_data() {
     d3.json("json_data/samples.json").then(function(sample_data) {
 
     //Push Objects into universal arrays
@@ -15,25 +15,6 @@ function optionChanged() {
     console.log(sample);
 //=================================================================
 
-    // //slice array so it's not a long line...
-    // function sliced_values(name_list) {
-    //     let iter_value = name_list;
-    //     let sliced_name = [];
-        
-    //     for (let nm = 0; nm < sliced_name.length; nm++) {
-    //         let n_index = iter_value[nm];
-    //         if (sliced_name.indexOf(n_index) == -1) {
-    //             sliced_name.push(n_index);
-    //         }
-    //     }
-    //     return sliced_name;
-    // }
-
-//=================================================================
-    //Makes the array vertical hopefully...
-    // let v_names_array = sliced_values(name_id);
-    // console.log(v_names_array);
-//=================================================================
     //Append data into dropdown list
     function append_dropdown(value) {
         
@@ -48,30 +29,20 @@ function optionChanged() {
 
     // //Load dropdown
     append_dropdown(names);
-
-    // let selection = d3.select("#selDataset").node().value;
-    // //Update values on change
-    // function change_value(v) {
-    //     let selection_value = v.node().value;
-    //     console.log(selection_value);
-    // }
-
-    // change_value(selection);
-
 });
 }
-    
+
 
 let selection = d3.select("#selDataset").node().value;
 
 //Update values on change
-function change_value(v) {
+function optionChanged(v) {
     let selection_value = v.value;
     console.log(selection_value);
 
-    optionChanged();
+    belly_data();
 }
 
-change_value(selection);
+optionChanged(selection);
 
 
