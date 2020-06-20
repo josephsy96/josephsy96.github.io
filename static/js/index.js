85067a2e28845ee0f107969ec5289d8f9c815997
josephsy96 <josephsy96@hotmail.com>
2020-06-20 13:16:17 -0500

    check mobile view

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,11 @@
-//load to the top when refreshing
+//On load animations
+history.scrollRestoration = "manual";
+
 window.onbeforeunload = function() {
-    window.scrollTo(0,0);
+    window.scrollTo(0,0); //on refresh the page starts at the top
 }
 
-//to animate text
+//to rotate the hashtags
 let text_rotation = function(el, rotation, period) {
     this.toRotate = rotation;
     this.el = el;
@@ -12,8 +14,9 @@ let text_rotation = function(el, rotation, period) {
     this.txt = "";
     this.tick();
     this.isDeleting = false;
-};
+}
 
+//iteration for the hashtags
 text_rotation.prototype.tick = function() {
     let i = this.loopNum % this.toRotate.length;
     let message = this.toRotate[i];
@@ -28,15 +31,15 @@ text_rotation.prototype.tick = function() {
     this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
     let that = this;
-    let delta = 300 - Math.random() * 100;
+    let delta = 350 - Math.random() * 100;
 
-    if (this.isDeleting) {delta /= 2};
-    
+    if(this.isDeleting) {delta /= 2};
+
     if(!this.isDeleting && this.txt === message) {
         delta = this.period;
-        this.isDeleting = true;
+        this.isDeleting= true;
     }
-    else if (this.isDeleting && this.txt === '') {
+    else if(this.isDeleting && this.txt === '') {
         this.isDeleting = 0;
         this.loopNum++;
         delta = 400;
@@ -44,24 +47,20 @@ text_rotation.prototype.tick = function() {
 
     setTimeout(function(){
         that.tick();
-    }, delta);
-};
+    },delta);
+}
 
-//Onload animations
+//onload animations
 window.onload = function() {
+    // document.getElementById("motto").classList.toggle("active");
 
-    // document.getElementById("open-text").classList.toggle('active');
-    
     let elements = document.getElementsByClassName("dif-text");
- 
-    for (let n = 0; n < elements.length; n++) {
+
+    for(let n = 0; n < elements.length; n++) {
         let newRotation = elements[n].getAttribute("data-rotate");
         let newPeriod = elements[n].getAttribute("data-rotate");
         if(newRotation) {
-            new text_rotation(elements[n], JSON.parse(newRotation),newPeriod);
+            new text_rotation(elements[n],JSON.parse(newRotation), newPeriod);
         }
     }
-    //animate "Photo" box
-    document.getElementById("box").classList.toggle('active');
-    this.document.getElementById("line").classList.toggle('active');
 };
